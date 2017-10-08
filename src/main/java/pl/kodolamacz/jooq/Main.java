f1b027caff18891c97d6a1ce05fe15ae1e6cbb9b
Krzysztof Koziol <kkoziol@gmail.com>
2017-10-08 13:18:13 +0200

    fly way

--- src/main/java/pl/kodolamacz/jooq/Main.java
+++ src/main/java/pl/kodolamacz/jooq/Main.java
@@ -1,11 +1,12 @@
 package pl.kodolamacz.jooq;
 
 //For convenience, always static import your generated tables and jOOQ functions to decrease verbosity:
-import static pl.kodolamacz.generated.Tables.NOTEBOOKS;
-import static pl.kodolamacz.generated.Tables.USERS;
+import static pl.kodolamacz.generated.Tables.AUTHOR;
+import static pl.kodolamacz.generated.Tables.BOOK;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import org.jooq.DSLContext;
 import org.jooq.Record;
@@ -15,36 +16,54 @@ import org.jooq.SelectConditionStep;
 import org.jooq.impl.DSL;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		String userName = "sa";
 		String password = "";
-		String url = "jdbc:h2:~/test";
+		String url = "jdbc:h2:~/flyway-test";
 
 		// Connection is the only JDBC resource that we need
 		// PreparedStatement and ResultSet are handled by jOOQ, internally
 		try (Connection conn = DriverManager.getConnection(url, userName, password)) {
 
 			DSLContext create = DSL.using(conn, SQLDialect.H2);
-			
-			Result<Record> result = create.select().from(NOTEBOOKS).fetch();
+			Result<Record> result = create.select().from(AUTHOR).fetch();
 
 			for (Record r : result) {
-				Integer id = r.getValue(NOTEBOOKS.ID);
-				String model = r.getValue(NOTEBOOKS.MODEL);
-				int resolution = r.getValue(NOTEBOOKS.RESOLUTION);
+				Integer id = r.getValue(AUTHOR.ID);
+				String firstName = r.getValue(AUTHOR.FIRST_NAME);
+				String lastName = r.getValue(AUTHOR.LAST_NAME);
 
-				System.out.println("ID: " + id + " model: " + model + " resolution: " + resolution);
+				System.out.println("ID: " + id + " first name: " + firstName + " last name: " + lastName);
 			}
 
-//			SelectConditionStep<Record> select = create.select().from(USER).where(USER.USER_ID.le(10));
-//			System.out.println(select.getSQL());
-//			Result<Record> fetch = select.fetch();
-//			System.out.println(fetch.formatInsert());
 		}
-
+		
 		// For the sake of this tutorial, let's keep exception handling simple
 		catch (Exception e) {
 			e.printStackTrace();
 		}
+
+		
+		
+		try (Connection c =
+	        		DriverManager.getConnection("jdbc:h2:~/flyway-test", "sa", "")) {
+	        		            Result<?> result =
+	        		            DSL.using(c)
+	        		               .select(
+	        		                   AUTHOR.FIRST_NAME,
+	        		                   AUTHOR.LAST_NAME,
+	        		                   BOOK.ID,
+	        		                   BOOK.LONG_TITLE
+	        		               )
+	        		               .from(AUTHOR)
+	        		               .join(BOOK)
+	        		               .on(AUTHOR.ID.eq(BOOK.AUTHOR_ID))
+	        		               .orderBy(BOOK.ID.asc())
+	        		               .fetch();
+
+	        		            System.out.println(result);
+	        		        }
+			
+			
 	}
 }
---
+++ src/main/resources/db/migration/V5__rename_column.sql
@@ -0,0 +1,2 @@
+-- V4__rename_column.sql
+ALTER TABLE flyway_test.book ALTER COLUMN title RENAME TO long_title;
